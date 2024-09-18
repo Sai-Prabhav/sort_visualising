8e273f59969127a9ab7d4b75b9dcb6a5e3d33381
Sai Prabhav <saiprabhav.ss@gmail.com>
2024-09-18 10:38:29 +0530

    added random sort

--- sort.c
+++ sort.c
@@ -5,22 +5,43 @@
 #include "array.h"
 #include "sketch.h"
 
-#define n 150
-int arr[n ];
+#define n 10
+int arr[n];
 int size = 1260;
+int number_steps = 0;
 
+void print_int(int x, char label[])
+{
+    printf("%s %d\n", label, x);
+}
 void draw(int j)
 {
-    clear_sketch();
-    draw_chart(arr, n, j + 1);
+    // clear_sketch();
+    // draw_chart(arr, n, j + 1);
+    number_steps += 1;
     // print_array(arr, n);
     // save_sketch("sketch.svg");
-    // usleep(100090);
-    save_frame();
+    // usleep(100000);
+    // save_frame();
 }
+
+int is_sorted(int arr[n])
+{
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void bubble_sort(int arr[n])
 {
     int temp;
+    printf("%d\n", is_sorted(arr));
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n - i - 1; j++)
@@ -36,13 +57,43 @@ void bubble_sort(int arr[n])
     }
 }
 
+void random_sort(int arr[n])
+{
+
+    int index;
+    int temp;
+    while (!is_sorted(arr))
+    {
+        index = rand() % (n - 1);
+        temp = arr[index];
+        arr[index] = arr[index + 1];
+        arr[index + 1] = temp;
+        draw(index);
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    set_sketch_size(size);
-    set_fill("#000000");
-    srand(time(0));
+    double min_seed=0, min_steps=100000000000000;
+    for (int s = 0; s < 10; s++)
+    {   
+        number_steps=0;
+        int seed = s*s+234%s;
+        print_int(seed, "seed: ");
+        srand(seed);
+        rand_array(arr, n);
+        random_sort(arr);
+        print_int(number_steps, "number of steps:");
+        if (number_steps<min_steps)
+        {
+            min_seed=seed;
+            min_steps=number_steps;
+        }
+        
+        print_int(min_steps,"the lowest steps is");
+        print_int(min_seed,"The seed of the fastest");
+    
+    }
 
-    rand_array(arr, n);
-    bubble_sort(arr);
     return 0;
 }
